6a005189670d3ccf48ccdb0ea3ff7217b0f594bd
JuanBautistaF <hidalgojb.23@gmail.com>
2022-03-07 11:00:08 -0600

    hmm

--- Autrum.py
+++ Autrum.py
@@ -16,6 +16,28 @@ detener = 0
 pausa = 0
 savedData = []
 
+# Numero de fotogramas en la cual se dividen las senales
+CHUNK = 2048  # tomo 2048 y proceso, luego tomo otros 2048 y asi sucesivamente
+
+# Me ayuda a establece que tan buena es una muestra de audio (Bit depth)
+FORMAT = pa.paInt16  # El mejor que soporta este equipo es el de 16 y el paALSA (Solo Linux)
+
+# El de 8 bits genera distorsion
+# El de 16bits (96db) es el que mas se utiliza, hace que se escuche mejor
+# El de 24bits es el que se usa actualmente y es mejor.
+# Esta de 32bits pero es muy alto para usarlo y nadie lo usa
+
+# Numero de canales de entrada a usar
+CHANNELS = 1  # Se usa uno ya que esto depende de la cantidad de entradas de audio que posee el equipo.
+
+# El RATE es la frecuencia de muestreo, o sea, el numero de muestras por una unidad de tiempo
+RATE = 44100  # (En HZ) Esta da una buena calidad, es la que normalemnte se usa (estandar)
+# La de 48000 da una calidad casi de estudio, (muy buena) pero depende del equipo.
+
+
+# Creacion de clase de PyAudio
+p = pa.PyAudio()
+
 
 def pulsa(tecla):
     if tecla == kb.KeyCode.from_char('i'):
@@ -43,11 +65,8 @@ def suelta(tecla):
 
     # Se presiona el boton de detener grabacion
     elif tecla == kb.KeyCode.from_char('d'):
-        if iniciar == 1 and pausa == 1:
+        if iniciar == 1:
             # pr = 0
-            detener = 1
-
-        elif iniciar == 1 and pausa == 0:
             detener = 1
 
     # Se presiona el boton de pausa/reanudar
@@ -56,59 +75,41 @@ def suelta(tecla):
             pausa = pausa ^ 1
 
 
-# Numero de fotogramas en la cual se dividen las senales
-CHUNK = 2048  # tomo 2048 y proceso, luego tomo otros 2048 y asi sucesivamente
-
-# Me ayuda a establece que tan buena es una muestra de audio (Bit depth)
-FORMAT = pa.paInt16  # El mejor que soporta este equipo es el de 16 y el paALSA (Solo Linux)
-
-# El de 8 bits genera distorsion
-# El de 16bits (96db) es el que mas se utiliza, hace que se escuche mejor
-# El de 24bits es el que se usa actualmente y es mejor.
-# Esta de 32bits pero es muy alto para usarlo y nadie lo usa
-
-# Numero de canales de entrada a usar
-CHANNELS = 1  # Se usa uno ya que esto depende de la cantidad de entradas de audio que posee el equipo.
-
-# El RATE es la frecuencia de muestreo, o sea, el numero de muestras por una unidad de tiempo
-RATE = 44100  # (En HZ) Esta da una buena calidad, es la que normalemnte se usa (estandar)
-# La de 48000 da una calidad casi de estudio, (muy buena) pero depende del equipo.
-
-
-# Creacion de clase de PyAudio
-p = pa.PyAudio()
-
 def test(filename):
     global p
     p = pa.PyAudio()
-    dbfile = open(filename, 'rb')     
+    dbfile = open(filename, 'rb')
     db = pickle.load(dbfile)
     dbfile.close()
 
     # Open a .Stream object to write the WAV file to
     # 'output = True' indicates that the sound will be played rather than recorded
-    stream = p.open(format = FORMAT, channels = CHANNELS, rate = RATE, output = True)
+    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True)
 
     # Play the sound by writing the audio data to the stream
     data = db[1]
-    i=1
+    i = 1
     print(len(data))
-    while i <= math.ceil(len(data)/CHUNK):
-        min_i = (i-1)*CHUNK
+    while i <= math.ceil(len(data) / CHUNK):
+        min_i = (i - 1) * CHUNK
         max_i = i * CHUNK
-        if max_i > len(data)-1:
-            max_i = len(data)-1
+        if max_i > len(data) - 1:
+            max_i = len(data) - 1
         stream.write(data)
-        i+=1
+        i += 1
 
     # Close and terminate the stream
     stream.close()
     p.terminate()
 
+
 def Analizador():
     # Esta funcion me toma una senal por medio del microfono y la mete una parte en un chunk
 
     frames = []  # Esto me ayuda a guardar los cuadros de la grabacion
+
+    dataInt = []
+    dataInt2 = []
 
     global detener
     global iniciar
@@ -117,7 +118,6 @@ def Analizador():
 
     escuchador = kb.Listener(pulsa, suelta)
     escuchador.start()
-    # escuchador.join()
 
     # Tomo la senal captada por el microfono, tomo un fracmento.
     entradaDeMic = p.open(
@@ -143,20 +143,15 @@ def Analizador():
     ax1.set_xlim(0, CHUNK)
     ax1.set_ylim(-40000, 40000)
 
-    ax2.set_xlim(20, RATE)
+    ax2.set_xlim(20, RATE + 200)
     # La salida de los calculos de furier varian de 0 a 1, meto -1 para verla mejor
-    ax2.set_ylim(-1, 1)
+    ax2.set_ylim(0, 2)
 
     line_frecuencia, = ax1.plot(x_frecuencia, np.random.rand(CHUNK), 'r')
     # La representacion de frecuencia siempre se hace en graficos semilogaritmicos
     line_furier, = ax2.semilogx(x_furier, np.random.rand(CHUNK), 'b')
 
     fig.show()
-
-    # Pa
-    dataInt2 = []
-
-    
 
     while escuchador.is_alive():
 
@@ -185,7 +180,7 @@ def Analizador():
         # aca trasnformamos la senal con furier
         temp_fft = np.abs(np.fft.fft(dataInt)) * 2 / (11000 * CHUNK)
         if iniciar:
-            savedData.append([temp_fft,dataInt])
+            savedData.append([temp_fft, dataInt])
         line_furier.set_ydata(temp_fft)
 
         fig.canvas.draw()
@@ -206,7 +201,8 @@ def Analizador():
 
         fig.canvas.draw()
         fig.canvas.flush_events()
-    
+
+
 def guardarGrabacion(frames):
     # Guardo el audio grabado
     # os.system("clear")
@@ -220,11 +216,12 @@ def guardarGrabacion(frames):
     gb.writeframes(b''.join(frames))
     gb.close()
 
-    db = [savedData,frames]
+    db = [savedData, frames]
     pickle.dump(db, dbfile)
 
 
 Analizador()
+
 
 def Reproductor():
     print("jijija")
